ee67d14a549489a5fde62549bf0de557ac6d6bd6
Garrett McGowan <garrett.m.mcgowan@gmail.com>
2018-08-06 20:21:28 -0500

    select/deselect funtionality

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ let MOCK_DATA = {
 
     items: [
         {
-            name: 'item1',
+            name: 'item12',
             cost: '200g',
             category: 'magical power',
             health: '+250',
@@ -10,21 +10,21 @@ let MOCK_DATA = {
             passive: 'This is the items passive.  Items usually have passives once completed',
             },
         {
-            name: 'item2',
+            name: 'item23',
             cost: '300g',
             category: 'physical power',
             movementspeed: '10%',
             power: '+40',
             },
         {
-            name: 'item3',
+            name: 'item34',
             cost: '3300g',
             category: 'physical defense',
             attackspeed: '10%',
             power: '+40',
             },
         {
-            name: 'item4',
+            name: 'item45',
             cost: '2200g',
             category: 'magical defense',
             cooldownpercentage: '20%',
@@ -32,7 +32,7 @@ let MOCK_DATA = {
             passive: 'This is the items passive.  Items usually have passives once completed',
             },
         {
-            name: 'item5',
+            name: 'item56',
             cost: '300g',
             category: 'utility',
             movementspeed: '10%',
@@ -41,7 +41,7 @@ let MOCK_DATA = {
             passive: 'This is the items passive.  Items usually have passives once completed',
             },
         {
-            name: 'item6',
+            name: 'item67',
             cost: '1000g',
             category: 'physical power',
             power: '+40',
@@ -58,38 +58,62 @@ let MOCK_DATA = {
             },
         ],
 };
+let inputTracker = 1;
 
-function handleItemsButton() {
-    $('.items-button').on('click', function () {
-        const names = MOCK_DATA.items.map(obj => obj.name);
-        //        console.log('this is names line 16: ' + names);
-        const namesHTML = generateNamesHTML(names);
-        //        console.log('this is names html line 19: ' + namesHTML);
-        $('.items-container').html(namesHTML);
-    })
-}
-$(handleItemsButton);
+function renderItems() {
+    const names = MOCK_DATA.items.map(obj => obj.name);
+    //        console.log('this is names line 16: ' + names);
+    const namesHTML = generateNamesHTML(names);
+    //        console.log('this is names html line 19: ' + namesHTML);
+    $('.items-container').html(namesHTML);
+};
 
 function generateNamesHTML(names) {
     return names.map(name => {
-        return `<p>${name}</p>`;
+        return `<p class="item">${name}</p>`;
     }).join('');
 }
 
+function collapsibleWindow() {
+
+}
+
 function selectItem() {
+    $('.items-container').on('click', '.item', function () {
+        if (inputTracker < 7) {
+            const itemText = $(this).text();
+            console.log(itemText);
+            let input = $('.build-form').find(`[data-attr='${inputTracker}']`);
+            $(input).val(itemText);
+            inputTracker++;
+        };
+        //        else call a function to display error to delete an input.
+    });
+}
+
+function removeInput() {
+    $('.item-input').on('click', function (event) {
+        if (inputTracker === 1) {
+            return;
+        };
+        $(this).val('');
+        inputTracker--;
+    });
 
 }
 
-function itemHover() {
-    $("p").hover(function () {
-            $(this).css("background-color", "yellow");
-        },
-        function () {
-            $(this).css("background-color", "pink");
-        });
-}
-$(itemHover);
 
-function filterItems() {
 
-}
+//function collapsibleWindow() {
+//  $(document).ready(function() {
+//Slide up and down on click
+//$("a.box-toggle").click(function(){
+//    $(this).toggleClass('active');
+//    $(this).parent().next("div.box-content").slideToggle("slow");
+//});
+//});
+//}
+$(generateNamesHTML);
+$(selectItem);
+$(removeInput);
+$(renderItems);
