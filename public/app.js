e7c27bdf2150e2655c27f6bebb8cee4957139cee
Garrett McGowan <garrett.m.mcgowan@gmail.com>
2018-08-29 18:15:36 -0500

    created filter functionality / updated look and feel

--- public/app.js
+++ public/app.js
@@ -5,46 +5,58 @@ let MOCK_DATA = {
             name: 'item12',
             cost: '200g',
             category: 'magical power',
-            health: '+250',
-            power: '+30',
-            passive: 'This is the items passive.  Items usually have passives once completed',
+            stats: {
+                health: '+250',
+                power: '+30',
+                passive: 'This is the items passive.  Items usually have passives once completed',
+            },
             },
         {
             name: 'item23',
             cost: '300g',
             category: 'physical power',
-            movementspeed: '10%',
-            power: '+40',
+            stats: {
+                movementspeed: '10%',
+                power: '+40',
+            },
             },
         {
             name: 'item34',
             cost: '3300g',
-            category: 'physical defense',
-            attackspeed: '10%',
-            power: '+40',
+            category: 'defense',
+            stats: {
+                attackspeed: '10%',
+                power: '+40',
             },
+        },
         {
             name: 'item45',
             cost: '2200g',
-            category: 'magical defense',
-            cooldownpercentage: '20%',
-            power: '+40',
+            category: 'defense',
+            stats: {
+                cooldownpercentage: '20%',
+                power: '+40',
+            },
             passive: 'This is the items passive.  Items usually have passives once completed',
             },
         {
             name: 'item56',
             cost: '300g',
-            category: 'utility',
-            movementspeed: '10%',
-            penetration: '+10',
-            health: '+200',
+            category: 'defense',
+            stats: {
+                movementspeed: '10%',
+                penetration: '+10',
+                health: '+200',
+            },
             passive: 'This is the items passive.  Items usually have passives once completed',
             },
         {
             name: 'item67',
             cost: '1000g',
             category: 'physical power',
-            power: '+40',
+            stats: {
+                power: '+40',
+            },
             passive: 'This is the items passive.  Items usually have passives once completed',
             },
 
@@ -58,30 +70,52 @@ let MOCK_DATA = {
             },
         ],
 };
+//global filter variables to track state of filter
+let magicalFilter = false;
+let physicalFilter = false;
+let defenseFilter = false;
+let utilityFilter = false;
+//global variable used to keep track of current input
 let inputTracker = 1;
-
-function renderItems() {
-    const names = MOCK_DATA.items.map(obj => obj.name);
-    //        console.log('this is names line 16: ' + names);
-    const namesHTML = generateNamesHTML(names);
-    //        console.log('this is names html line 19: ' + namesHTML);
-    $('.items-container').html(namesHTML);
-};
-
-function generateNamesHTML(names) {
-    return names.map(name => {
-        return `<p class="item">${name}</p>`;
+//Render items
+function renderItems(data) {
+    data.items.forEach(item => {
+        $('.items-container').append(generateHTML(item));
+    })
+}
+//Generate item HTML
+function generateHTML(item) {
+    return `
+<li class="item-box">
+    <div class="name-select-box">
+        <button class="select-button">Select</button>
+        <p class="item-name">${item.name}</p>
+    </div>
+    <div class="collapsible">
+        <p class="item-cost">${item.cost}</p>
+        <p class="item-category">${item.category}</p>
+        ${generateHTMLStats(item.stats)}
+    </div>
+</li>
+`;
+}
+//Generate item stats HTML
+function generateHTMLStats(stats) {
+    return Object.keys(stats).map(key => {
+        return `<p class="item-value">${stats[key]}<p>`
     }).join('');
 }
-
-function collapsibleWindow() {
-
+//Collapsible item window
+function collapsibleItemWindow() {
+    $('.items-container').on('click', '.item-name', function () {
+        $(this).parent().next().toggleClass("active");
+    });
 }
-
+//Selected items are placed into input slots
 function selectItem() {
-    $('.items-container').on('click', '.item', function () {
+    $('.name-select-box').on('click', '.select-button', function () {
         if (inputTracker < 7) {
-            const itemText = $(this).text();
+            const itemText = $(this).next().text();
             console.log(itemText);
             let input = $('.build-form').find(`[data-attr='${inputTracker}']`);
             $(input).val(itemText);
@@ -90,7 +124,7 @@ function selectItem() {
         //        else call a function to display error to delete an input.
     });
 }
-
+//Clear input value
 function removeInput() {
     $('.item-input').on('click', function (event) {
         if (inputTracker === 1) {
@@ -99,21 +133,92 @@ function removeInput() {
         $(this).val('');
         inputTracker--;
     });
-
 }
+//Filter for magical items
+function filterMagicalItems() {
+    $('.magical-button').on("click", function () {
+        magicalFilter = !magicalFilter;
+        if (magicalFilter === true) {
+
+            $('.item-box').toArray().forEach(function (item) {
+
+                if ($(item).find('.item-category').text() !== "magical power") {
+                    $(item).css("display", "none");
+                } else {
+                    $(item).css("display", "block");
+                };
+            });
+        } else {
+            $('.item-box').toArray().forEach(function (item) {
+                if ($(item).find('.item-category').text() === "magical power") {
+                    $(item).css("display", "block");
+                } else {
+                    $(item).css("display", "block");
+                };
+            });
+        };
+    });
+};
+
+function filterPhysicalItems() {
+    $('.physical-button').on("click", function () {
+        physicalFilter = !physicalFilter;
+        if (physicalFilter === true) {
+
+            $('.item-box').toArray().forEach(function (item) {
+
+                if ($(item).find('.item-category').text() !== "physical power") {
+                    $(item).css("display", "none");
+                } else {
+                    $(item).css("display", "block");
+                };
+            });
+        } else {
+            $('.item-box').toArray().forEach(function (item) {
+                if ($(item).find('.item-category').text() === "physical power") {
+                    $(item).css("display", "block");
+                } else {
+                    $(item).css("display", "block");
+                };
+            });
+        };
+    });
+};
+//this function is not working yet (not finding magical defense category)
+function filterDefenseItems() {
+    $('.defense-button').on("click", function () {
+        defenseFilter = !defenseFilter;
+        if (defenseFilter === true) {
+
+            $('.item-box').toArray().forEach(function (item) {
+
+                if ($(item).find('.item-category').text() !== "defense") {
+                    $(item).css("display", "none");
+
+                } else {
+                    $(item).css("display", "block");
+                };
+            })
+        } else {
+            $('.item-box').toArray().forEach(function (item) {
+                if ($(item).find('.item-category').text() === "defense") {
+                    $(item).css("display", "block");
+                } else {
+                    $(item).css("display", "block");
+                };
+            });
+        };
+    });
+};
 
 
 
-//function collapsibleWindow() {
-//  $(document).ready(function() {
-//Slide up and down on click
-//$("a.box-toggle").click(function(){
-//    $(this).toggleClass('active');
-//    $(this).parent().next("div.box-content").slideToggle("slow");
-//});
-//});
-//}
-$(generateNamesHTML);
+
+
+$(filterDefenseItems);
+$(filterPhysicalItems);
+$(filterMagicalItems);
+$(collapsibleItemWindow);
 $(selectItem);
 $(removeInput);
-$(renderItems);
+$(renderItems(MOCK_DATA));
--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'It works!'
+    });
+});
 app.listen(process.env.PORT || 8080);
